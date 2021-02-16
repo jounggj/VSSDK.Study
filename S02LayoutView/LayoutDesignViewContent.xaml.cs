62609b2a6c40d9575d9267b48bb12223f159c32b
GyongJin Joung <jounggj@gmail.com>
2021-02-16 23:34:10 +0900

    Added UnityHWndHost.cs to hold an actual Win32 window.

--- S02LayoutView/LayoutDesignViewContent.xaml.cs
+++ S02LayoutView/LayoutDesignViewContent.xaml.cs
@@ -20,23 +20,35 @@ namespace S02LayoutView
             this.InitializeComponent();
         }
 
+        private Process process = null;
+
         private void LaunchLayoutDesignViewer(object sender, EventArgs e)
         {
             System.Console.WriteLine("LaunchLayoutDesignViewer:  called.");
 
             Window layoutWindow = Window.GetWindow(borderLayoutView);
-            IntPtr hWnd = new WindowInteropHelper(layoutWindow).EnsureHandle();
+            //IntPtr hWnd = new WindowInteropHelper(layoutWindow).EnsureHandle();
+            IntPtr hWnd = new WindowInteropHelper(layoutWindow).Handle;
 
             string viewExecutablePath = "E:\\Work\\VSSDK.Study\\S02LayoutView\\SampleHelloWorldBuild\\SampleHelloWorld.exe";
-            ProcessStartInfo startInfo = new ProcessStartInfo(viewExecutablePath);
-            string commandArguments = String.Format("-parentHWND {0:d} delayed", hWnd);
-            //startInfo.Arguments = commandArguments;
-            Process.Start(startInfo);
+            //string commandArguments = String.Format("-parentHWND {0:d} delayed", hWnd);
+            string commandArguments = String.Format("-parentHWND {0}", hWnd);
+
+            process = new Process();
+            process.StartInfo.FileName = viewExecutablePath;
+            process.StartInfo.Arguments = commandArguments;
+            process.StartInfo.UseShellExecute = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            process.WaitForInputIdle();
         }
 
         private void btnTest_Click(object sender, RoutedEventArgs e)
         {
-            LaunchLayoutDesignViewer(this, e);
+            //LaunchLayoutDesignViewer(this, e);
+
+            string viewExecutablePath = "E:\\Work\\VSSDK.Study\\S02LayoutView\\SampleHelloWorldBuild\\SampleHelloWorld.exe";
+            borderLayoutView.Child = new UnityHWndHost(viewExecutablePath, "");
         }
     }
 }
